627839603532d7c681cc5affd679321c26785c84
JCKalt <ckaltwasser@reminingcorp.com>
2023-02-23 16:09:26 -0500

    redraw function added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [drawState, setDrawState] = useState(false);
   const [start, setStart] = useState({ x: 0, y: 0 });
   const [end, setEnd] = useState({ x: 0, y: 0 });
+  const [rectangles, setRectangles] = useState([]);
 
   const onMouseDown = (e) => {
     const rect = canvasRef.current.getBoundingClientRect();
@@ -18,42 +19,42 @@ function App() {
     setEnd({ x, y });
   };
 
-const onMouseMove = (e) => {
-  if (!drawState) {
-    return;
-  }
+  const onMouseMove = (e) => {
+    if (!drawState) {
+      return;
+    }
 
-  const rect = canvasRef.current.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+    const rect = canvasRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
 
-  const context = canvasRef.current.getContext("2d");
-  context.setLineDash([5, 5]);
+    const context = canvasRef.current.getContext("2d");
+    context.setLineDash([5, 5]);
 
-  // Clear previous dashed border
-  context.clearRect(start.x, start.y, end.x - start.x, end.y - start.y);
+    // Clear previous dashed border
+    context.clearRect(start.x, start.y, end.x - start.x, end.y - start.y);
 
-  // Draw white rectangle over previous dashed border
-  context.beginPath();
-  context.fillStyle = "#ffffff";
-  context.rect(start.x + 1, start.y + 1, end.x - start.x - 2, end.y - start.y - 2);
-  context.fill();
-  context.closePath();
+    // Redraw all rectangles
+    redrawRectsIfNeeded();
 
-  // Update end coordinates and draw new dashed border
-  setEnd({ x, y });
-  context.beginPath();
-  context.strokeStyle = color;
-  context.rect(start.x + 1, start.y + 1, x - start.x - 2, y - start.y - 2);
-  context.stroke();
-  context.closePath();
-};
+    // Update end coordinates and draw new dashed border
+    setEnd({ x, y });
+    context.beginPath();
+    context.strokeStyle = color;
+    context.rect(start.x + 1, start.y + 1, x - start.x - 2, y - start.y - 2);
+    context.stroke();
+    context.closePath();
+  };
 
   const onMouseUp = (e) => {
     setDrawState(false);
     const rect = canvasRef.current.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
+
+    // Redraw all rectangles
+    redrawRectsIfNeeded();
+
     const context = canvasRef.current.getContext("2d");
     context.setLineDash([]);
     context.beginPath();
@@ -61,9 +62,32 @@ const onMouseMove = (e) => {
     context.fillStyle = color;
     context.fill();
     context.closePath();
+
+    setRectangles([...rectangles, { x: start.x, y: start.y, width: x - start.x, height: y - start.y, redrawNeeded: false }]);
   };
 
-  return (
+  const redrawRectsIfNeeded = () => {
+    rectangles.forEach((rectangle, index) => {
+      if (rectangle.redrawNeeded) {
+        const context = canvasRef.current.getContext("2d");
+        context.beginPath();
+        context.clearRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        context.stroke();
+        rectangles[index].redrawNeeded = false;
+      }
+    });
+  };
+
+  const onColorChange = (e) => {
+    setColor(e.target.value);
+  };
+
+  const onRadiusChange = (e) => {
+    setRadius(e.target.value);
+  };
+
+return (
     <>
       <div className="controls">
         <input
